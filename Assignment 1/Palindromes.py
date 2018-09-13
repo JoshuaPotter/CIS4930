8708b227824af64fcd174ad8156704cdbb6f814e
Joshua Potter <tljoshh@gmail.com>
2018-09-13 19:47:52 -0400

    Pascals triangle

--- Assignment 1/Palindromes.py
+++ Assignment 1/Palindromes.py
@@ -3,6 +3,8 @@
     Palindromes.py '''
 
 def palindromic(word):
+    ''' Remove white space, convert to lower case, and compare
+        word frontward and backward '''
     if word[::-1].lower().replace(" ","") == word.lower().replace(" ",""):
         return True
     else:
@@ -12,12 +14,17 @@ if __name__ == "__main__":
     i = 1
     wordList = {}
     userInput = input("Enter the strings: ")
+
+    ''' Calls plaindromic() to determine if word
+        should be added with key to wordList dictionary, then
+        asks for another input value to check '''
     while userInput != "Done":
         if palindromic(userInput):
             wordList[i] = userInput
             i += 1
         userInput = input()
 
+    ''' Prints the list if there are palindromes found '''
     if len(wordList) > 0:
         print(wordList)
     else:
--- Assignment 1/Pascal.py
+++ Assignment 1/Pascal.py
@@ -1,3 +1,25 @@
 ''' Joshua Potter, jop13
     Pascal.py '''
 
+def coefficient(row, elem):
+    coefficient = 1
+
+    if(row - elem < elem):
+        elem = row - elem
+
+    for index in range(0, elem):
+        coefficient = coefficient * (row - index)
+        coefficient = coefficient // (1 + index)
+    
+    return coefficient
+
+def printTriangle(N):
+    for row in range(0, N):
+        print()
+        for rowElement in range(0, row + 1):
+            print(" ", coefficient(row, rowElement), end = "")
+    print()
+
+if __name__ == "__main__":
+    N = int(input("Enter the number of rows: "))
+    printTriangle(N)
