3945e6435c20ae9230eac845eab856074e95f9e7
Joshua Potter <tljoshh@gmail.com>
2018-09-15 20:32:44 -0400

    Final commit

--- Assignment 1/Palindromes.py
+++ Assignment 1/Palindromes.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python 
-''' Joshua Potter, jop13
-    Palindromes.py '''
+'''
+    Joshua Potter, jop13
+    Palindromes.py 
+'''
 
 def palindromic(word):
-    ''' Remove white space, convert to lower case, and compare
-        word frontward and backward '''
+    ''' 
+        Remove white space, convert to lower case, and compare
+        word frontward and backward 
+    '''
     if word[::-1].lower().replace(" ","") == word.lower().replace(" ",""):
         return True
     else:
@@ -15,17 +18,20 @@ if __name__ == "__main__":
     wordList = {}
     userInput = input("Enter the strings: ")
 
-    ''' Calls plaindromic() to determine if word
+    ''' 
+        Calls plaindromic() to determine if word
         should be added with key to wordList dictionary, then
-        asks for another input value to check '''
+        asks for another input value to check 
+    '''
     while userInput != "Done":
         if palindromic(userInput):
             wordList[i] = userInput
             i += 1
         userInput = input()
 
-    ''' Prints the list if there are palindromes found '''
+    # Prints the list if there are palindromes found 
     if len(wordList) > 0:
+        print("The palindromes are:")
         print(wordList)
     else:
         print("No palindromes found.")
--- Assignment 1/Pascal.py
+++ Assignment 1/Pascal.py
@@ -1,24 +1,33 @@
-''' Joshua Potter, jop13
-    Pascal.py '''
+''' 
+    Joshua Potter, jop13
+    Pascal.py 
+'''
 
-def coefficient(row, elem):
-    coefficient = 1
-
-    if(row - elem < elem):
-        elem = row - elem
-
-    for index in range(0, elem):
-        coefficient = coefficient * (row - index)
-        coefficient = coefficient // (1 + index)
-    
-    return coefficient
-
-def printTriangle(N):
-    for row in range(0, N):
+def printTriangle(rows):
+    '''
+        Print new line for each row, then 
+        print each value and a space
+    '''
+    for row in range(0, rows):
         print()
         for rowElement in range(0, row + 1):
-            print(" ", coefficient(row, rowElement), end = "")
-    print()
+            print(coefficient(row, rowElement), end = "")
+            print(" ", end = "")
+
+def coefficient(row, elem):
+    '''
+        Calculate element position, then calculate
+        element binomial coeffecient value
+    '''
+    coefficient = 1
+    # Element position
+    if(row - elem < elem):
+        elem = row - elem
+    # Binomial coefficient
+    for index in range(0, elem):
+        coefficient = coefficient * (row - index)
+        coefficient = coefficient // (index + 1)
+    return coefficient
 
 if __name__ == "__main__":
     N = int(input("Enter the number of rows: "))
