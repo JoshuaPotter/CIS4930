e54eccb6ed74bb2f3632c98a0a0d6609c24d6058
Joshua Potter <tljoshh@gmail.com>
2018-10-15 23:35:29 -0400

    Added totient function

--- Assignment 2/RSA.py
+++ Assignment 2/RSA.py
@@ -1,31 +1,55 @@
+import random
+
 class RSA(object):
+   # Constructor
    def __init__(self):
       self.list = []
       self.e = 0
       self.d = 0
 
+   # Get messages from user
    def inputFunc(self):
+      # Get the number of entries
       numberOfMessages = input("Enter the number of messages: ")
       numberOfMessages = int(numberOfMessages)
+
+      # Get the entries and append to object list
       print("Enter the messages: ")
       while numberOfMessages > 0:
          self.list.append(input())
          numberOfMessages = numberOfMessages - 1
 
-   def printFunc(self):
-      print(self.list)      
+   # Print message along with number parameter
+   def printFunc(self, number):
+      print("The message is", number)
 
+   # Generate the next prime number after min parameter
    def primeGen(self, min):
-      print()
+      isPrime = False
+      if min == 1:
+         prime = 2
+      else:
+         # Iterate past "min" number until we find a prime
+         iterator = min + 1
+         while isPrime == False:
+            index = 2
+            while index < iterator:
+               if iterator % index == 0:
+                  isPrime = False
+                  index = iterator
+               else:
+                  isPrime = True
+               index = index + 1
+            iterator = iterator + 1
+         prime = iterator - 1
+      return prime
 
+   # Generate RSA keys from prime numbers
    def keyGen(self, min):
-      print()
-
-   def LCM(self):
-      print()
-
-   def GCD(self):
-      print()
+      p = self.primeGen(min)
+      q = self.primeGen(p)
+      N = p * q
+      self.e = random.randint(1, self.totient(p, q))
 
    def encrypt(self, messageToEncrypt):
       print()
@@ -36,9 +60,27 @@ class RSA(object):
    def messages(self):
       print()
 
+   '''
+   Helper Functions
+   '''
+
+   # Lowest common multiple
+   def LCM(self):
+      print()
+
+   # Greatest common denominator 
+   def GCD(self):
+      print()
+   
+   # Calculates phi of N
+   def totient(self, p, q):
+      return ((p-1)*(q-1))
+
+
 # run program
 if __name__ == "__main__":
    algorithm = RSA()
-   algorithm.inputFunc()
-   algorithm.printFunc()
-
+   #  algorithm.inputFunc()
+   #  algorithm.printFunc(5)
+   x = 205
+   print("The next prime number to", x, "is", algorithm.primeGen(x))
