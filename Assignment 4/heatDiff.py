d8b4fd4d4e361e98ec4c370f2b5c8dd5b5562d38
Joshua Potter <tljoshh@gmail.com>
2018-12-07 17:17:41 -0500

    clusting

--- Assignment 4/heatDiff.py
+++ Assignment 4/heatDiff.py
@@ -12,8 +12,6 @@ if __name__ == "__main__":
    for i in range(0,int(temperature)+2):
       currentState[i,0] = temperature
 
-   #print(currentState)
-
    n = 0
    while n < 3000 and not np.array_equal(previousState, currentState):
       previousState = np.copy(currentState)
@@ -23,33 +21,26 @@ if __name__ == "__main__":
             currentState[i,j] = .25 * (previousState[i-1, j] + previousState[i+1, j] + previousState[i, j-1] + previousState[i, j+1])
             j += 1
       n += 1
-   print(currentState)
-   #axes.axis([0,int(temperature),0,int(temperature)])
 
-   # section = temperature // 8
-   # for i in range(1,int(temperature)):        # columns
-   #    for j in range(1,int(temperature)):     # rows
-   #       if currentState[i,j] < section:
-   #          plt.scatter(j,i,c="darkblue")
-   #       elif currentState[i,j]*2 <= section:
-   #          plt.scatter(j,i,c="blue")
-   #       elif currentState[i,j]*3 <= section:
-   #          plt.scatter(j,i,c="aqua")
-   #       elif currentState[i,j]*4 <= section:
-   #          plt.scatter(j,i,c="lawngreen")
-   #       elif currentState[i,j]*5 <= section:
-   #          plt.scatter(j,i,c="yellow")
-   #       elif currentState[i,j]*6 <= section:
-   #          plt.scatter(j,i,c="orange")
-   #       elif currentState[i,j]*7 <= section:
-   #          plt.scatter(j,i,c="red")
-   #       else:
-   #          plt.scatter(j,i,c="darkred")
+   section = temperature // 8
+   for i in range(1,int(temperature)):        # columns
+      for j in range(1,int(temperature)):     # rows
+         if currentState[i,j] <= section:
+            plt.scatter(j,i,c="darkblue")
+         elif section < currentState[i,j] < section*2:
+            plt.scatter(j,i,c="blue")
+         elif section*2 < currentState[i,j] < section*3:
+            plt.scatter(j,i,c="aqua")
+         elif section*3 < currentState[i,j] < section*4:
+            plt.scatter(j,i,c="lawngreen")
+         elif section*4 < currentState[i,j] < section*5:
+            plt.scatter(j,i,c="yellow")
+         elif section*5 < currentState[i,j] < section*6:
+            plt.scatter(j,i,c="orange")
+         elif section*6 < currentState[i,j] < section*7:
+            plt.scatter(j,i,c="red")
+         else:
+            plt.scatter(j,i,c="darkred")
 
    plt.title("Heat Diffusion")
-   plt.show()
-
-   figure, axes = plt.subplots()
-   data = axes.imshow(currentState, cmap=plt.get_cmap('hot'), interpolation='nearest', vmin=0, vmax=temperature)
-   figure.colorbar(data)
    plt.show()
---
+++ Assignment 4/iris.py
@@ -0,0 +1,61 @@
+''' This program demostrates clustering on the iris dataset
+    The iris dataset is a dataset about 3 kinds of iris flowers, with 4 features
+    per flower. 
+    Clustering is a type of machine learning where there is no idea of the ground 
+    truth. Instead, data points are grouped as "similar" based on how close
+    they are to each other.
+
+    The sklearn library contains a lot of functions for machine learning,
+    including clustering '''
+
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cluster import KMeans
+import sklearn.metrics as sm
+from sklearn.decomposition import PCA
+import pandas as pd
+import numpy as np
+
+# The sklearn library come prepackaged with a bunch of datasets. We are loading the iris dataset
+
+digits = datasets.load_digits()
+
+#print(digits.data)
+
+pca = PCA(2)
+projected = pca.fit_transform(digits.data)
+
+''' pandas is a library that organizes data nicely. We are formatting the
+    dataset into a pandas dataframe. Then we name the features. '''
+
+x = pd.DataFrame(digits.data)
+#x.columns = ['Sepal_L', 'Sepal_W', 'Petal_L', 'Petal_W']
+
+''' Though clustering does not require the ground truth, the dataset is 
+    labeled anyway. So we can check the results of clustering against the answer.
+    So, we load the 'answer' into a separate data frame '''
+
+y = pd.DataFrame(digits.target)
+# y.columns = ['Targets']
+
+# this line actually builds the machine learning model and runs the algorithm
+# on the dataset
+model = KMeans(n_clusters =10)
+model.fit(x)
+
+#print(model.labels_)
+
+#plot the 2 graphs side to side
+plt.figure(111)
+
+colormap = np.array(['darkred', 'red', 'darkblue', 'blue', 'green', 'lawngreen', 'yellow', 'orange', 'purple', 'black'])
+
+# plt.subplot(1, 2, 1)
+# plt.scatter(x.Petal_L, x.Petal_W, c=colormap[y.Targets], s=40)
+# plt.title('Real Classification')
+ 
+# Plot the Models Classifications
+plt.scatter(projected[:, 0], projected[:, 1], c=colormap[model.labels_], s=40)
+plt.title('K Means Classification')
+
+plt.show()
